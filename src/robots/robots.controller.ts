958670c96c792c81cebc9fb702386df6e7f49270
Darlene <spacebiscuit@mac.home>
2026-08-14 13:39:37 +0200

    Add GET findAllRobots endpoint

--- src/robots/robots.controller.ts
+++ src/robots/robots.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RobotsService } from './robots.service';
 
 @Controller('robots')
-export class RobotsController {}
+export class RobotsController {
+  constructor(private readonly RobotService: RobotsService) {}
+
+  @Get()
+  findAll() {
+    return this.RobotService.findAllRobots();
+  }
+}
--- src/robots/robots.service.ts
+++ src/robots/robots.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Robot } from './entities/robot.entity';
+import { RobotStatus } from './entities/robot-status.entity';
 
 @Injectable()
-export class RobotsService {}
+export class RobotsService {
+  constructor(
+    @InjectRepository(Robot)
+    private readonly robotRepo: Repository<Robot>,
+    @InjectRepository(RobotStatus)
+    private readonly statusRepo: Repository<RobotStatus>,
+  ) {}
+
+  async findAllRobots(): Promise<Robot[]> {
+    return this.robotRepo.find();
+  }
+
+  async createStatus() {}
+
+  async findStatusHistory() {}
+}
