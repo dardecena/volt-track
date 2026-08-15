402cc0ee46258c169ec0cece6fd15e022de3c907
Darlene <spacebiscuit@mac.home>
2026-08-15 14:13:11 +0200

    FE: Add mdi and vuetify to app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,7 @@
+import '@mdi/font/css/materialdesignicons.css';
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { vuetify } from "./plugins/vuetify.ts";
 
-createApp(App).mount('#app')
+createApp(App).use(vuetify).mount('#app')
