45a32e6784f3b8f6bd5cbdeb9dc138cf9c240da4
Darlene <spacebiscuit@mac.home>
2026-08-15 14:13:26 +0200

    FE: Initial layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+ <v-app>
+   <v-navigation-drawer color="red" width="240">
+     <div class="d-flex align-center ga-2 pa-4">
+       <v-avatar size="32" color="primary">
+         <v-icon icon="mdi-lightning-bolt" color="white" size="20" />
+       </v-avatar>
+       <span class="text-h6 font-weight-bold">VoltTrack</span>
+     </div>
+   </v-navigation-drawer>
+   <v-app-bar color="green"></v-app-bar>
+   <v-main></v-main>
+ </v-app>
 </template>
