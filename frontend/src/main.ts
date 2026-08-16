971a455e71b77d7b4882627d7f01dc1ec370e85e
Darlene <darlene.decena@gmail.com>
2026-08-16 15:32:06 +0200

    FE: Update order of imports in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
-import '@mdi/font/css/materialdesignicons.css';
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 import { vuetify } from "./plugins/vuetify.ts";
+
+import '@mdi/font/css/materialdesignicons.css';
+
 
 createApp(App).use(vuetify).mount('#app')
